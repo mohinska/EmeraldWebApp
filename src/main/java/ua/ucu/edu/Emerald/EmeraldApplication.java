ad8746b60fa99f6b9b52df70b297f81d7d51e46e
Ivan Zarytskyi <ivan.zarytskyi1991@gmail.com>
2025-11-07 12:49:04 +0200

    Init Springboot

---
+++ src/main/java/ua/ucu/edu/Emerald/EmeraldApplication.java
@@ -0,0 +1,13 @@
+package ua.ucu.edu.Emerald;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmeraldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmeraldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/ucu/edu/Emerald/EmeraldApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ucu.edu.Emerald;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmeraldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
